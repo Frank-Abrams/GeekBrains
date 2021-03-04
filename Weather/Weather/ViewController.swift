fd399363e4f9d54db4684ec3d8824d84d993742f
Frank-Abrams <frank-abrams@users.noreply.github.com>
2021-03-04 20:58:27 +0200

    Lesson 1 files updated

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Frank Abrams on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginScrollView: UIScrollView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButtonAction(_ sender: Any) {
+        
+        // Получаем текст логина
+        let login = loginTextField.text!
+        
+        // Получаем текст-пароль
+        let password = passwordTextField.text!
+                
+        // Проверяем, верны ли они
+        if login == "admin" && password == "123456" {
+            print("Авторизация успешная")
+        } else {
+            print("Авторизация не удалась")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Жест нажатия
+        let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        
+        // Присваиваем его UIScrollVIew
+        loginScrollView?.addGestureRecognizer(hideKeyboardGesture)
+
+    }
+
+    // Когда клавиатура появляется
+    @objc func keyboardWasShown(notification: Notification) {
+        
+        // Получаем размер клавиатуры
+        let info = notification.userInfo! as NSDictionary
+        let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        
+        // Добавляем отступ внизу UIScrollView, равный размеру клавиатуры
+        self.loginScrollView?.contentInset = contentInsets
+        loginScrollView?.scrollIndicatorInsets = contentInsets
+    }
+    
+    //Когда клавиатура исчезает
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        
+        // Устанавливаем отступ внизу UIScrollView, равный 0
+        let contentInsets = UIEdgeInsets.zero
+        loginScrollView?.contentInset = contentInsets
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Подписываемся на два уведомления: одно приходит при появлении клавиатуры
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWasShown), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        // Второе — когда она пропадает
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillBeHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+            
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func hideKeyboard() {
+        self.loginScrollView?.endEditing(true)
+    }
+
+}
+
